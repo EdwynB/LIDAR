df89d4394ece6c63b02f1bd9d39323c5e3579d87
Sylvain Bentz <sylvain.bentz@centrale-marseille.fr>
2018-09-28 15:15:16 +0200

    ca fait beaucoup de commandes quand meme

---
+++ lidar.c
@@ -0,0 +1,16 @@
+char SYNC_BYTE = "\xA5";
+char SYNC_BYTE2 = "\x5A";
+
+char GET_INFO_BYTE = "\50";
+char HEALTH_BYTE = "\x52";
+char SAMPLE_RATE_BYTE = "\x59";
+
+char STOP_BYTE = "\x25";
+char RESET_BYTE = "\x40";
+
+char SCAN_BYTE = "\x20";
+
+int response,size;
+char byte;
+
+_SCAN_TYPE tableau[3] = ["normal" :  ]
